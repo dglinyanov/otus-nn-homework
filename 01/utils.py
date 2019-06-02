4408bba88fabaf873a8ec7ad0772e8e355759947
Dmitry Glinyanov <gliv321@gmail.com>
2019-06-03 01:07:15 +0700

    01. Updated transform to use whole dataset mean and std instead of single picture data.

--- 01/utils.py
+++ 01/utils.py
@@ -6,26 +6,57 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def custom_normalize(image):
-    mean = image.mean()
-    std = image.std()
-    return transforms.functional.normalize(image, (mean, ), (std, ))
+def get_dataset_mean_std(dataset):
+    loader = utils.data.DataLoader(
+        dataset,
+        batch_size=1000,
+        num_workers=4,
+        shuffle=False
+    )
 
-def custom_check(image):
-    print(image.mean(), image.std())
-    return image
+
+    mean = 0.
+    std = 0.
+    nb_samples = 0.
+    for data, data_y in loader:
+        batch_samples = data.size(0)
+        data = data.view(batch_samples, data.size(1), -1)
+        mean += data.mean(2).sum(0)
+        std += data.std(2).sum(0)
+        nb_samples += batch_samples
+
+    mean /= nb_samples
+    std /= nb_samples
     
-mnist_transform = transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Lambda(custom_normalize),
-                transforms.Lambda(custom_check),
-           ])
+    return mean, std
 
-def mnist(batch_size=50, shuffle=True, transform=mnist_transform, path='./MNIST_data'):
+
+mnist_pre_transform = transforms.Compose([
+    transforms.ToTensor()
+])
+
+def mnist(batch_size=50, shuffle=True, transform=mnist_pre_transform, path='./MNIST_data'):
     train_data = datasets.MNIST(path, train=True, download=True, transform=transform)
+    train_mean, train_std = get_dataset_mean_std(train_data)
+    print('Train mean and std', train_mean, train_std)
+    mnist_train_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((train_mean, ), (train_std,))
+    ])
+    train_data = datasets.MNIST(path, train=True, download=True, transform=mnist_train_transform)
+
     test_data = datasets.MNIST(path, train=False, download=True, transform=transform)
+    test_mean, test_std = get_dataset_mean_std(test_data)
+    print('Test mean and std', test_mean, test_std)
+    mnist_test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((test_mean, ), (test_std,))
+    ])
+    test_data = datasets.MNIST(path, train=False, download=True, transform=mnist_test_transform)
+    
     train_loader = utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=shuffle)
     test_loader = utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=shuffle)
+    
     return train_loader, test_loader
 
 def plot_mnist(images, shape):
